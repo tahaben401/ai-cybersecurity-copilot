43e084457fd0b4d14d6867fcdeaf96bdd130fdca
tahaben401 <bentaha2004@gmail.com>
2026-03-25 21:55:38 +0100

    Initial commit

---
+++ src/main/java/com/example/aicybersecuritycopilot/AiCybersecurityCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicybersecuritycopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCybersecurityCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCybersecurityCopilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aicybersecuritycopilot/AiCybersecurityCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicybersecuritycopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCybersecurityCopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
